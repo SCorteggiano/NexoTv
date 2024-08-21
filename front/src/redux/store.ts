0e41e10204fd28867cc4cdfd1065acc7e5af0db0
SCorteggiano <stefano.corteggiano@gmail.com>
2024-08-21 17:37:56 -0300

    Redux provider fix

--- front/src/redux/store.ts
+++ front/src/redux/store.ts
@@ -1,8 +1,9 @@
 import { configureStore } from '@reduxjs/toolkit'
+import authReducer from './reducer'
 
 export const store = configureStore({
   reducer: {
-    
+    auth: authReducer,
   },
 })
 
