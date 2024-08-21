0f74cf79d0a36344ae844e801d115f4ac8d05f8f
Diego Carreon <diego24carreon@gmail.com>
2024-08-21 15:28:32 -0600

    Movies Respaldo

--- front/src/app/movies/page.tsx
+++ front/src/app/movies/page.tsx
@@ -1,8 +1,23 @@
+import MovieCard from '@/components/MovieCard/MovieCard'
 import React from 'react'
 
 const Movies = () => {
   return (
-    <div>Movies</div>
+    <div>
+      <div className='border h-12 flex items-center justify-around'>
+        <h1>CATEGORY1</h1>
+        <h1>CATEGORY2</h1>
+        <h1>CATEGORY3</h1>
+        <h1>CATEGORY4</h1>
+        <h1>CATEGORY5</h1>
+      </div>
+
+      <div id='wholeContainer' className='border m-6'>
+        <div id='movieContainer'>
+          <MovieCard/>
+        </div>
+      </div>
+    </div>
   )
 }
 
---
+++ front/src/components/MovieCard/MovieCard.tsx
@@ -0,0 +1,16 @@
+/* eslint-disable @next/next/no-img-element */
+import React from "react";
+
+const MovieCard = () => {
+  return (
+    <div id="movieContainer">
+      <div className="flex-row">
+        <img src="" alt="movieImage" />
+        <h1>Movie Title</h1>
+
+      </div>
+    </div>
+  );
+};
+
+export default MovieCard;
