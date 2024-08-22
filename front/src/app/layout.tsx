36ac8626422b8af31a14af78cd1b214f3bc4b150
Mauricio Tognoli <tognolimauricio@gmail.com>
2024-08-21 23:31:41 -0300

    configuracion de toolkit

--- front/src/app/layout.tsx
+++ front/src/app/layout.tsx
@@ -1,8 +1,7 @@
 import type { Metadata } from "next";
 import { Montserrat } from "next/font/google";
 import "./globals.css";
-import { Provider } from "react-redux";
-import { store } from "@/redux/store";
+import { Providers } from "@/redux/providers";
 import Navbar1 from "@/components/Navbar1/Navbar1";
 import Navbar2 from "@/components/Navbar2/Navbar2";
 
@@ -21,11 +20,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={montserrat.className}>
-        {/* Agregar Provider */}
-        <Navbar1 />
-        <Navbar2 />
-        {children}
-        {/* Agregar Provider */}
+        <Providers>
+          <Navbar1 />
+          <Navbar2 />
+          {children}
+        </Providers>
       </body>
     </html>
   );
