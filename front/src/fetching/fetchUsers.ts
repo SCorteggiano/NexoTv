78dcdaec2cf7b801e68081b2328b14075fdd9434
SCorteggiano <stefano.corteggiano@gmail.com>
2024-08-22 21:07:25 -0300

    fetching modificado graphQL

--- front/src/fetching/fetchUsers.ts
+++ front/src/fetching/fetchUsers.ts
@@ -1,25 +1,56 @@
 import { IUser, ILoginUser } from "@/interfaces";
 
 export const postRegister = async (user: Omit<IUser, "id">) => {
-  const res = await fetch("...", {
+  const res = await fetch("TU_URL_DE_GRAPHQL", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
     },
-    body: JSON.stringify(user),
+    body: JSON.stringify({
+      query: `
+        mutation RegisterUser($input: RegisterInput!) {
+          registerUser(input: $input) {
+            id
+            name
+            email
+            token
+          }
+        }
+      `,
+      variables: {
+        input: user,
+      },
+    }),
   });
   const data = await res.json();
-  return data;
+  return data.data.registerUser;
 };
 
+
 export const postLogin = async (credentials: ILoginUser) => {
-  const res = await fetch("...", {
+  const res = await fetch("TU_URL_DE_GRAPHQL", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
     },
-    body: JSON.stringify(credentials),
+    body: JSON.stringify({
+      query: `
+        mutation LoginUser($input: LoginInput!) {
+          loginUser(input: $input) {
+            token
+            user {
+              id
+              name
+              email
+            }
+          }
+        }
+      `,
+      variables: {
+        input: credentials,
+      },
+    }),
   });
   const data = await res.json();
-  return data;
+  return data.data.loginUser;
 };
