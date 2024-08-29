f2e766957046948120f8bb1af5e7f3dd5fb381f8
SCorteggiano <stefano.corteggiano@gmail.com>
2024-08-28 23:49:16 -0300

    Config de Stripe + button + succes/cancel pages

---
+++ front/src/app/cancel/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Cancel = () => {
+  return (
+    <div>CANCELO LA OPERACION</div>
+  )
+}
+
+export default Cancel;
---
+++ front/src/app/succes/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Succes = () => {
+  return (
+    <div>LA OPERACION FUE EXITOSA</div>
+  )
+}
+
+export default Succes;
